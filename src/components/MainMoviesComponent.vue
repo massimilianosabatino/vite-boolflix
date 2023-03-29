8d62ad43249465d68ca5435e0a02188f23db3241
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-29 19:43:48 +0200

    add: if language has a flag show it

--- src/components/MainMoviesComponent.vue
+++ src/components/MainMoviesComponent.vue
@@ -6,8 +6,22 @@
         data(){
             return {
                 store,
+                flagList: ['it', 'en', 'de', 'es', 'fr', 'gb', 'us'],
             }
         },
+        methods: {
+            showFlag(lang){
+                if(this.flagList.includes(lang)){
+                    //const url = `"./../assets/img/flag/${lang}.svg"`;
+                    //const altText = `"Flag of ${lang}""`;
+                    const flag = `<img src="./src/assets/img/flag/${lang}.svg" alt="flag of ${lang}">`
+                    return flag
+                } else {
+                    const text = lang;
+                    return text
+                }
+            }
+        }
     }
 </script>
 
@@ -17,13 +31,15 @@
             {{ movie.title }}
             <ul>
                 <li>{{ movie.original_title }}</li>
-                <li>{{ movie.original_language }}</li>
+                <li v-html="showFlag(movie.original_language)" class="lang-container"></li>
                 <li>{{ movie.vote_average }}</li>
+                
             </ul>
         </li>
     </ul>
 </template>
 
 <style lang="scss" scoped>
+
 
 </style>
