4d19009a56dcc0893663a34f206ead14376d4f0d
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-04-01 20:11:15 +0200

    add: request genres list from api

--- src/components/MainAppComponent.vue
+++ src/components/MainAppComponent.vue
@@ -1,14 +1,42 @@
 <script>
+    import axios from 'axios';
+    import { store } from '../store';
     import MainMoviesComponent from './MainMoviesComponent.vue';
     import MainTvShowsComponent from './MainTvShowsComponent.vue';
     import MainMediaComponent from './MainMediaComponent.vue';
     export default {
         name: 'Main',
+        data(){
+            return {
+                store,
+            }
+        },
         components: {
             MainMoviesComponent,
             MainTvShowsComponent,
             MainMediaComponent,
         },
+        mounted(){
+            //Get genres from API
+            axios({
+                baseURL: this.store.utility.apiUrl,
+                url: this.store.utility.getGenresMovie,
+                params:{
+                    api_key: this.store.utility.apiKey,
+                    language: 'it-IT',
+                    query: this.store.searchKey,
+                }
+            }).then(response => this.store.genresMovie = response.data.genres);
+            axios({
+                baseURL: this.store.utility.apiUrl,
+                url: this.store.utility.getGenresTv,
+                params:{
+                    api_key: this.store.utility.apiKey,
+                    language: 'it-IT',
+                    query: this.store.searchKey,
+                }
+            }).then(response => this.store.genresTv = response.data.genres);
+        }
     }
 </script>
 
